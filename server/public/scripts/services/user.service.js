9c1f136dcc312cfcc76e9e92500453716b2a12c9
Tom Ritland <t_ritland@yahoo.com>
2017-09-28 14:53:33 -0500

    setup separate home and login views

--- server/public/scripts/services/user.service.js
+++ server/public/scripts/services/user.service.js
@@ -16,11 +16,11 @@ myApp.factory('UserService', function($http, $location){
           } else {
               console.log('UserService -- getuser -- failure');
               // user has no session, bounce them back to the login page
-              $location.path("/home");
+              $location.path("/login");
           }
       },function(response){
         console.log('UserService -- getuser -- failure: ', response);
-        $location.path("/home");
+        $location.path("/login");
       });
     },
 
@@ -28,7 +28,7 @@ myApp.factory('UserService', function($http, $location){
       console.log('UserService -- logout');
       $http.get('/user/logout').then(function(response) {
         console.log('UserService -- logout -- logged out');
-        $location.path("/home");
+        $location.path("/login");
       });
     }
   };
