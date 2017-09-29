9036855e0daeb96d15005a64c68f6d957e3fc97e
Robert Cordray <robertcordray@gmail.com>
2017-09-29 13:30:39 -0500

    admin views created, linked with ng-route

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -2,48 +2,60 @@ var myApp = angular.module('myApp', ['ngRoute']);
 
 /// Routes ///
 myApp.config(function($routeProvider, $locationProvider) {
-  $locationProvider.hashPrefix('');
-  console.log('myApp -- config')
-  $routeProvider
-    .when('/home', {
-      templateUrl: '/views/templates/home.html',
-      controller: 'LoginController as lc',
-    })
-    .when('/login', {
-      templateUrl: '/views/templates/login.html',
-      controller: 'LoginController as lc',
-    })
-    .when('/register', {
-      templateUrl: '/views/templates/register.html',
-      controller: 'LoginController as lc'
-    })
-    .when('/user', {
-      templateUrl: '/views/templates/user.html',
-      controller: 'UserController as uc',
-      resolve: {
-        getuser : function(UserService){
-          return UserService.getuser();
-        }
-      }
-    })
-    .when('/overview', {
-      templateUrl: '/views/templates/overview.html',
-      controller: 'LoginController as lc',
-    })
-    .when('/volunteerDetail', {
-      templateUrl: '/views/templates/volunteerDetail.html',
-      controller: 'LoginController as lc',
-    })
-    .when('/info', {
-      templateUrl: '/views/templates/info.html',
-      controller: 'InfoController',
-      resolve: {
-        getuser : function(UserService){
-          return UserService.getuser();
-        }
-      }
-    })
-    .otherwise({
-      redirectTo: 'home'
-    });
+    $locationProvider.hashPrefix('');
+    console.log('myApp -- config')
+    $routeProvider
+        .when('/home', {
+            templateUrl: '/views/templates/home.html',
+            controller: 'LoginController as lc',
+        })
+        .when('/login', {
+            templateUrl: '/views/templates/login.html',
+            controller: 'LoginController as lc',
+        })
+        .when('/register', {
+            templateUrl: '/views/templates/register.html',
+            controller: 'LoginController as lc'
+        })
+        .when('/user', {
+            templateUrl: '/views/templates/user.html',
+            controller: 'UserController as uc',
+            resolve: {
+                getuser: function(UserService) {
+                    return UserService.getuser();
+                }
+            }
+        })
+        .when('/overview', {
+            templateUrl: '/views/templates/overview.html',
+            controller: 'LoginController as lc',
+        })
+        .when('/volunteerDetail', {
+            templateUrl: '/views/templates/volunteerDetail.html',
+            controller: 'LoginController as lc',
+        })
+        .when('/info', {
+            templateUrl: '/views/templates/info.html',
+            controller: 'InfoController',
+            resolve: {
+                getuser: function(UserService) {
+                    return UserService.getuser();
+                }
+            }
+        })
+        .when('/manageadmin', {
+            templateUrl: '/views/manageadmin.html',
+            controller: 'ManageAdminController as vm',
+        })
+        .when('/newadmin', {
+            templateUrl: '/views/newadmin.html',
+            controller: 'newAdminController as vm',
+        })
+        .when('/training', {
+            templateUrl: '/views/training.html',
+            controller: 'trainingController as vm',
+        })
+        .otherwise({
+            redirectTo: 'home'
+        });
 });
---
+++ server/public/scripts/controllers/manageadmin.controller.js
@@ -0,0 +1,4 @@
+myApp.controller('ManageAdminController', ['UserService', function(UserService) {
+    console.log('ManageAdminController created');
+    var vm = this;
+}])
