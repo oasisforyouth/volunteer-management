390d7e1a6a57c7c5c3a65a88a7954503a31adc34
polygeekism <mcmahon.kevin.t@gmail.com>
2017-10-02 15:26:37 -0500

    all volunteers object and get route created

--- server/public/scripts/controllers/overview.controller.js
+++ server/public/scripts/controllers/overview.controller.js
@@ -1,4 +1,7 @@
 myApp.controller('OverviewController', function($http, $location, UserService) {
     console.log('OverviewController created');
     var self = this;
+    self.allVolunteers = VolunteerService.allVolunteers;
+
+    VolunteerService.getAllVolunteers();
 });
