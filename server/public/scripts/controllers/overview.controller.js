53220d7aa46ecb6030520d94b1b9aa95f3eecb72
Tom Ritland <t_ritland@yahoo.com>
2017-10-02 10:08:06 -0500

    added getVolunteer function to overview controller

--- server/public/scripts/controllers/overview.controller.js
+++ server/public/scripts/controllers/overview.controller.js
@@ -1,4 +1,13 @@
-myApp.controller('OverviewController', function($http, $location, UserService) {
+myApp.controller('OverviewController', ['VolunteerService', function(VolunteerService, $http, $location, UserService) {
     console.log('OverviewController created');
     var vm = this;
-});
+    vm.volunteerData = VolunteerService.volunteerData;
+
+
+    vm.getVolunteers = function(){
+        VolunteerService.getVolunteers();
+    }
+
+
+    vm.getVolunteers();
+}]);
