38f22ebfc4dc8ec79dee442d960ac22fddda15c7
polygeekism <mcmahon.kevin.t@gmail.com>
2017-10-03 14:17:17 -0500

    ng-model roughed out.

--- server/public/scripts/controllers/application.controller.js
+++ server/public/scripts/controllers/application.controller.js
@@ -1,6 +1,7 @@
 myApp.controller('ApplicationController', ['$http', '$location', 'UserService', 'VolunteerService',  function($http, $location, UserService, VolunteerService) {
     console.log('ApplicationController created');
     var self = this;
+    self.newApplication = {};
 
     VolunteerService.postNewVolunteer();
 
