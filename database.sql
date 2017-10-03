49afc343c2434717f1fc2785be226253e56a8b0f
polygeekism <mcmahon.kevin.t@gmail.com>
2017-10-03 14:28:11 -0500

    sql table fixing

--- database.sql
+++ database.sql
@@ -1,35 +1,20 @@
 db name: oasis
 
+	
 create table Users (
-	id SERIAL PRIMARY KEY,
-	first_name TEXT NOT NULL,
-	last_name TEXT NOT NULL,
-	user_name TEXT NOT NULL,
-	email TEXT NOT NULL,
+    id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
+    last_name TEXT NOT NULL,
+    user_name TEXT NOT NULL,
+    email TEXT NOT NULL,
     password VARCHAR(100),
     position TEXT NOT NULL
-	);
---this password is "1" for both admins	
-INSERT INTO users (first_name, last_name, user_name, email, password, position)
-VALUES ('John', 'Doe', 'John', 'johndoe@themail.com','$2a$10$b2QDCEBm/twSssJ7jPNObO8rbTISTLbArfQHaYAhfWVtjUmg.HceG', 'IT Administrator'),
-	('Jane', 'Joe', 'Jane', 'janejoe@themail.com', '$2a$10$b2QDCEBm/twSssJ7jPNObO8rbTISTLbArfQHaYAhfWVtjUmg.HceG','Social Worker');
-
---update both users with a position
-UPDATE "public"."users" SET "position"='Social Worker' WHERE "id"=2 RETURNING "id", "first_name", "last_name", "user_name", "email", "password", "position";
-UPDATE "public"."users" SET "position"='IT Administrator' WHERE "id"=1 RETURNING "id", "first_name", "last_name", "user_name", "email", "password", "position";
-
---first volunteer
-INSERT INTO "public"."volunteers"("first_name", "last_name", "middle_name", "address", "time_at_address", "primary_phone", "secondary_phone", "email", "preferred_contact", "emergency_name", "emergency_phone", "emergency_address", "emergency_relationship", "drivers_license", "reference_1_name", "reference_1_address", " reference_1_phone", "reference_1_email", "reference_1_years_known", "reference_1_relationship", "additional_hobbies", "volunteer_interests", "admin_notes", "tutoring_skill", "cooking_skill", "art_skill", "health_wellness_skill", "career_jobs_skill", "tutor_opportunity", "dropin_opportunity", "transportation_opportunity", "administration_opportunity", "marketing_opportunity", "supplies_donations_opportunity", "fundraising_events_opportunity", "english", "spanish", "hours_any", "no_day_preference", "monday_preference", "tuesday_preference", "wednesday_preference", "attended_orientation", "orientation_date") VALUES('James', 'Peach', 'Charlie', '1234 Scranton Way, Bloomington MN', '3 years', '6517633456', '6517635647', 'james.peach@themail.com', 'primary phone', 'Mary Peach', '6122344321', '1234 Scranton Way, Bloomington MN', 'Wife', TRUE, 'Tyler Burkhead', '654 Sprinfield Dr, Edina', '6518912345', 'tyler@workplace.com', '15 ', 'Childhood friend', 'Underwater basket weaving, Podcast', 'coach sports teams', 'has interview scheduled', TRUE, FALSE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, TRUE, FALSE, TRUE, FALSE, TRUE, TRUE, TRUE, FALSE, TRUE, TRUE, FALSE, TRUE, '10/15/16');
-
--- need to run this to add new field to your database
-    ALTER TABLE "public"."volunteers"
-  ADD COLUMN "attended_orientation" boolean DEFAULT false,
-  ADD COLUMN "orientation_date" date;
-
-
---this takes out a column that is not needed or delete the volunteers table and run that code snippet below again
-  ALTER TABLE "public"."volunteers" DROP COLUMN "languages";
+    );
 	
+	INSERT INTO users (first_name, last_name, user_name, email, password, position)
+VALUES ('John', 'Doe', 'John', 'johndoe@themail.com','$2a$10$b2QDCEBm/twSssJ7jPNObO8rbTISTLbArfQHaYAhfWVtjUmg.HceG', 'IT Administrator'),
+    ('Jane', 'Joe', 'Jane', 'janejoe@themail.com', '$2a$10$b2QDCEBm/twSssJ7jPNObO8rbTISTLbArfQHaYAhfWVtjUmg.HceG','Social Worker');
+
 create table Trainings (
 	id SERIAL PRIMARY KEY,
 	title TEXT NOT NULL,
@@ -48,8 +33,8 @@ create table Interests (
 	);
 	
 CREATE TABLE Volunteers (
-	id SERIAL PRIMARY KEY,
-	first_name TEXT NOT NULL,
+    id SERIAL PRIMARY KEY,
+    first_name TEXT NOT NULL,
     last_name TEXT NOT NULL,
     middle_name TEXT,
     maiden_name TEXT,
@@ -66,13 +51,13 @@ CREATE TABLE Volunteers (
     drivers_license BOOLEAN NOT NULL,
     reference_1_name TEXT NOT NULL,
     reference_1_address TEXT NOT NULL,
-    reference_1_phone TEXT NOT NULL,
+    reference_1_phone VARCHAR(10) NOT NULL,
     reference_1_email TEXT,
     reference_1_years_known TEXT NOT NULL,
     reference_1_relationship TEXT NOT NULL,
     reference_2_name TEXT,
     reference_2_address TEXT,
-    reference_2_phone TEXT,
+    reference_2_phone VARCHAR(10),
     reference_2_email TEXT,
     reference_2_years_known TEXT,
     reference_2_relationship TEXT,
@@ -107,7 +92,7 @@ CREATE TABLE Volunteers (
     friday_preference BOOLEAN DEFAULT FALSE,
     saturday_preference BOOLEAN DEFAULT FALSE,
     sunday_preference BOOLEAN DEFAULT FALSE,
-    attended_orientation boolean DEFAULT false,
-    orientation_date date;
-
+    attended_orientation BOOLEAN DEFAULT false,
+    orientation_date date,
+    admin_notes TEXT
 );
