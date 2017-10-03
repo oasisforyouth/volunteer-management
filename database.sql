d2a80306d5729fa1182c8a2fd9f3622237dba4d2
polygeekism <mcmahon.kevin.t@gmail.com>
2017-10-03 12:13:45 -0500

    sql statements updated

--- database.sql
+++ database.sql
@@ -6,12 +6,13 @@ create table Users (
 	last_name TEXT NOT NULL,
 	user_name TEXT NOT NULL,
 	email TEXT NOT NULL,
-    password VARCHAR(100)
+    password VARCHAR(100),
+    position TEXT NOT NULL
 	);
 --this password is "1" for both admins	
-INSERT INTO users (first_name, last_name, user_name, email, password)
-VALUES ('John', 'Doe', 'John', 'johndoe@themail.com','$2a$10$b2QDCEBm/twSssJ7jPNObO8rbTISTLbArfQHaYAhfWVtjUmg.HceG'),
-	('Jane', 'Joe', 'Jane', 'janejoe@themail.com', '$2a$10$b2QDCEBm/twSssJ7jPNObO8rbTISTLbArfQHaYAhfWVtjUmg.HceG');
+INSERT INTO users (first_name, last_name, user_name, email, password, position)
+VALUES ('John', 'Doe', 'John', 'johndoe@themail.com','$2a$10$b2QDCEBm/twSssJ7jPNObO8rbTISTLbArfQHaYAhfWVtjUmg.HceG', 'IT Administrator'),
+	('Jane', 'Joe', 'Jane', 'janejoe@themail.com', '$2a$10$b2QDCEBm/twSssJ7jPNObO8rbTISTLbArfQHaYAhfWVtjUmg.HceG','Social Worker');
 
 --first volunteer
 INSERT INTO "public"."volunteers"("first_name", "last_name", "middle_name", "address", "time_at_address", "primary_phone", "secondary_phone", "email", "preferred_contact", "emergency_name", "emergency_phone", "emergency_address", "emergency_relationship", "drivers_license", "reference_1_name", "reference_1_address", " reference_1_phone", "reference_1_email", "reference_1_years_known", "reference_1_relationship", "additional_hobbies", "volunteer_interests", "admin_notes", "tutoring_skill", "cooking_skill", "art_skill", "health_wellness_skill", "career_jobs_skill", "tutor_opportunity", "dropin_opportunity", "transportation_opportunity", "administration_opportunity", "marketing_opportunity", "supplies_donations_opportunity", "fundraising_events_opportunity", "english", "spanish", "hours_any", "no_day_preference", "monday_preference", "tuesday_preference", "wednesday_preference", "attended_orientation", "orientation_date") VALUES('James', 'Peach', 'Charlie', '1234 Scranton Way, Bloomington MN', '3 years', '6517633456', '6517635647', 'james.peach@themail.com', 'primary phone', 'Mary Peach', '6122344321', '1234 Scranton Way, Bloomington MN', 'Wife', TRUE, 'Tyler Burkhead', '654 Sprinfield Dr, Edina', '6518912345', 'tyler@workplace.com', '15 ', 'Childhood friend', 'Underwater basket weaving, Podcast', 'coach sports teams', 'has interview scheduled', TRUE, FALSE, TRUE, TRUE, TRUE, TRUE, FALSE, FALSE, TRUE, FALSE, TRUE, FALSE, TRUE, TRUE, TRUE, FALSE, TRUE, TRUE, FALSE, TRUE, '10/15/16');
@@ -20,6 +21,7 @@ INSERT INTO "public"."volunteers"("first_name", "last_name", "middle_name", "add
     ALTER TABLE "public"."volunteers"
   ADD COLUMN "attended_orientation" boolean DEFAULT false,
   ADD COLUMN "orientation_date" date;
+
 
 --this takes out a column that is not needed or delete the volunteers table and run that code snippet below again
   ALTER TABLE "public"."volunteers" DROP COLUMN "languages";
