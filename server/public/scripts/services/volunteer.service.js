2d4b37ad305990fba841ebbe6e13cc9a444f8678
Nic Bade <chipchapmaniii@gmail.com>
2017-10-04 16:05:22 -0500

    nodemailer stuff

--- server/public/scripts/services/volunteer.service.js
+++ server/public/scripts/services/volunteer.service.js
@@ -1,16 +1,16 @@
-myApp.service('VolunteerService', function ($http, $location) {
+myApp.service('VolunteerService', function($http, $location) {
     console.log('volunteer service loaded');
     var self = this;
-    self.allVolunteers = {list:[]};
+    self.allVolunteers = { list: [] };
 
-    self.postNewVolunteer = function(newVolunteer){
+    self.postNewVolunteer = function(newVolunteer) {
         console.log('new volunteer object', newVolunteer);
-        $http.post('/volunteer', newVolunteer).then(function(response){
+        $http.post('/volunteer', newVolunteer).then(function(response) {
             console.log('completed the post route', response);
-        })
+        });
     }
-    self.getAllVolunteers = function(){
-        $http.get('/volunteer').then(function(response){
+    self.getAllVolunteers = function() {
+        $http.get('/volunteer').then(function(response) {
             console.log('all volunteers from server', response.data);
             self.allVolunteers.list = response.data;
 
