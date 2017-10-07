728b9d440429e845a241104c17aa11e7a785faa6
Nic Bade <chipchapmaniii@gmail.com>
2017-10-07 14:29:11 -0500

    set env on email information in email route

--- server/routes/email.router.js
+++ server/routes/email.router.js
@@ -4,7 +4,6 @@ let passport = require('passport');
 let path = require('path');
 let nodemailer = require('nodemailer');
 
-<<<<<<< HEAD
 router.post('/', (req, res, next) => {
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
@@ -14,27 +13,15 @@ router.post('/', (req, res, next) => {
             pass: process.env.DB_PASS
         }
     });
-=======
-    router.post('/', (req, res, next) => {
-        // create reusable transporter object using the default SMTP transport
-        let transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.DB_USER, 
-                pass: process.env.DB_PASS 
-            }
-        });
->>>>>>> 9e45a2cfc9e36a1e110bb925410bb5a77d61fbf7
 
     // setup email data with unicode symbols
     let mailOptions = {
-        from: '"Administrator" <pm.oasisforyouth@gmail.com>', // sender address  NEEDS ADDRESS
-        to: 'nicbade@me.com', // list of receivers NEEDS ADDRESS
+        from: '"Administrator"' + process.env.DB_EMAIL, // sender address  NEEDS ADDRESS
+        to: process.env.DB_PM_EMAIL, // list of receivers NEEDS ADDRESS
         subject: 'Volunteer Application', // Subject line
         text: 'A new Volunteer application has been submitted', // plain text body
     };
 
-<<<<<<< HEAD
     // send mail with defined transport object
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
@@ -49,10 +36,9 @@ router.post('/', (req, res, next) => {
 
     });
 });
-
 // AUTHENTICATE ME
 router.post('/user', (req, res, next) => {
-    let email = req.body;
+    var email = req.body;
     console.log('post req.body', email);
     // create reusable transporter object using the default SMTP transport
     let transporter = nodemailer.createTransport({
@@ -62,45 +48,16 @@ router.post('/user', (req, res, next) => {
             pass: process.env.DB_PASS
         }
     });
-=======
-        // send mail with defined transport object
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                res.sendStatus(500);
-                return console.log(error);
-            } else {
-                res.sendStatus(200);
-                console.log('Message sent: %s', info.messageId);
-                console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-    
-            }
-
-        });
-    });
-// AUTHENTICATE ME
-    router.post('/user', (req, res, next) => {
-        var email = req.body;
-        console.log('post req.body', email);
-        // create reusable transporter object using the default SMTP transport
-        let transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.DB_USER,
-                pass: process.env.DB_PASS 
-            }
-        });
->>>>>>> 9e45a2cfc9e36a1e110bb925410bb5a77d61fbf7
 
     // setup email data with unicode symbols
     let mailOptions = {
-        from: '"Administrator" <pm.oasisforyouth@gmail.com>', // sender address  NEEDS ADDRESS
+        from: '"Administrator" <DB_EMAIL>', // sender address  NEEDS ADDRESS
         // HOW TO DO THIS DYNAMICALLY
         to: email.email, // list of receivers NEEDS ADDRESS
         subject: 'New Admin', // Subject line
-        text: 'Please click the following link: ' + 'http://localhost:5000/#/newAdmin ', // plain text body
+        text: 'Please click the following link: ' + process.env.DB_HOST + '/#/newAdmin ', // plain text body
     };
 
-<<<<<<< HEAD
     // send mail with defined transport object
     transporter.sendMail(mailOptions, (error, info) => {
         if (error) {
@@ -108,16 +65,7 @@ router.post('/user', (req, res, next) => {
         }
         console.log('Message sent: %s', info.messageId);
         console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-=======
-        // send mail with defined transport object
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                return console.log(error);
-            }
-            console.log('Message sent: %s', info.messageId);
-            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-        });
->>>>>>> 9e45a2cfc9e36a1e110bb925410bb5a77d61fbf7
     });
+});
 
 module.exports = router;
--- server/routes/nodemailer.js
+++ server/routes/nodemailer.js
@@ -13,8 +13,8 @@ nodemailer.createTestAccount((err, account) => {
 
     // setup email data with unicode symbols
     let mailOptions = {
-        from: '"Fred Foo 👻" <pm.oasisforyouth@gmail.com>', // sender address  NEEDS ADDRESS
-        to: 'nicbade@me.com', // list of receivers NEEDS ADDRESS
+        from: '"Oasis for Youth" <DB_EMAIL>', // sender address  NEEDS ADDRESS
+        to: 'DB_PM_EMAIL', // list of receivers NEEDS ADDRESS
         subject: 'Hello ✔', // Subject line
         text: 'Hello world?', // plain text body
         html: '<b>Hello world?</b>' // html body
@@ -29,7 +29,5 @@ nodemailer.createTestAccount((err, account) => {
         // Preview only available when sending through an Ethereal account
         console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
 
-        // Message sent: <b658f8ca-6296-ccf4-8306-87d57a0b4321@blurdybloop.com>
-        // Preview URL: https://ethereal.email/message/WaQKMgKddxQDoou...
     });
 });
