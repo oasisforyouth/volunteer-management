1090b338e52c1ce4567840159ca8ea5f7b210356
Nic Bade <chipchapmaniii@gmail.com>
2017-10-07 14:56:55 -0500

    added user authentication to user Route

--- server/routes/email.router.js
+++ server/routes/email.router.js
@@ -4,63 +4,81 @@ let passport = require('passport');
 let path = require('path');
 let nodemailer = require('nodemailer');
 
+// NEW VOLUNTEER APPLICAITON EMAIL
 router.post('/', (req, res, next) => {
     // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.DB_USER,
-            pass: process.env.DB_PASS
-        }
-    });
+    if (req.isAuthenticated()) {
+        console.log('logged in email', req.user);
 
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: '"Administrator"' + process.env.DB_EMAIL, // sender address  NEEDS ADDRESS
-        to: process.env.DB_PM_EMAIL, // list of receivers NEEDS ADDRESS
-        subject: 'Volunteer Application', // Subject line
-        text: 'A new Volunteer application has been submitted', // plain text body
-    };
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.DB_USER,
+                pass: process.env.DB_PASS
+            }
+        });
 
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-    });
+        // setup email data with unicode symbols
+        let mailOptions = {
+            from: '"Administrator"' + process.env.DB_EMAIL, // sender address  
+            // THIS WILL BE JESS' EMAIL ADDRESS
+            to: process.env.DB_PM_EMAIL, // list of receivers 
+            subject: 'Volunteer Application', // Subject line
+            // JESS CAN PERSONALIZE THIS IF SHE WOULD LIKE US TO
+            text: 'A new Volunteer application has been submitted', // plain text body
+        };
+
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);
+            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        });
+    } else {
+        console.log('not logged in');
+        res.sendstatus(403);
+    }
 });
-// AUTHENTICATE ME
+
+
 router.post('/user', (req, res, next) => {
     var email = req.body;
     console.log('post req.body', email);
     // create reusable transporter object using the default SMTP transport
-    let transporter = nodemailer.createTransport({
-        service: 'gmail',
-        auth: {
-            user: process.env.DB_USER,
-            pass: process.env.DB_PASS
-        }
-    });
 
-    // setup email data with unicode symbols
-    let mailOptions = {
-        from: '"Administrator"' + process.env.DB_EMAIL, // sender address  NEEDS ADDRESS
-        // HOW TO DO THIS DYNAMICALLY
-        to: email.email, // list of receivers NEEDS ADDRESS
-        subject: 'New Admin', // Subject line
-        text: 'Please click the following link: ' + process.env.DB_HOST + '/#/newAdmin ', // plain text body
-    };
+    if (req.isAuthenticated()) {
+        console.log('logged in email', req.user);
 
-    // send mail with defined transport object
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            return console.log(error);
-        }
-        console.log('Message sent: %s', info.messageId);
-        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-    });
+        let transporter = nodemailer.createTransport({
+            service: 'gmail',
+            auth: {
+                user: process.env.DB_USER,
+                pass: process.env.DB_PASS
+            }
+        });
+
+        // setup email data with unicode symbols
+        let mailOptions = {
+            from: '"Administrator"' + process.env.DB_EMAIL, // sender address  NEEDS ADDRESS
+            to: email.email, // list of receivers NEEDS ADDRESS
+            subject: 'New Administrator', // Subject line
+            text: 'Please click the following link: ' + process.env.DB_HOST, // plain text body
+        };
+
+        // send mail with defined transport object
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);
+            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+        });
+    } else {
+        console.log('not logged in');
+        res.sendstatus(403);
+    }
 });
 
 module.exports = router;
--- server/routes/user.router.js
+++ server/routes/user.router.js
@@ -18,25 +18,34 @@ router.get('/', function(req, res) {
         // failure best handled on the server. do redirect here.
         console.log('not logged in');
         // should probably be res.sendStatus(403) and handled client-side, esp if this is an AJAX request (which is likely with AngularJS)
-        res.send(false);
+        res.sendStatus(403);
     }
 });
 router.get('/allusers', function(req, res) {
-    pool.connect(function(errorConnectingToDatabase, client, done) {
-        if (errorConnectingToDatabase) {
-            res.sendStatus(500);
-        } else {
-            client.query('SELECT first_name ,last_name, position, user_name, email, id FROM users', function(errorMakingQuery, result) {
-                done();
-                if (errorMakingQuery) {
-                    res.sendStatus(500);
-                } else {
-                    res.send(result.rows);
-                }
-            })
-        }
-    })
-})
+    if (req.isAuthenticated()) {
+        // send back user object from database
+        console.log('logged in', req.user);
+        pool.connect(function(errorConnectingToDatabase, client, done) {
+            if (errorConnectingToDatabase) {
+                res.sendStatus(500);
+            } else {
+                client.query('SELECT first_name ,last_name, position, user_name, email, id FROM users', function(errorMakingQuery, result) {
+                    done();
+                    if (errorMakingQuery) {
+                        res.sendStatus(500);
+                    } else {
+                        res.send(result.rows);
+                    }
+                })
+            }
+        })
+    } else {
+        // failure best handled on the server. do redirect here.
+        console.log('not logged in');
+        // should probably be res.sendStatus(403) and handled client-side, esp if this is an AJAX request (which is likely with AngularJS)
+        res.sendStatus(403);
+    }
+});
 
 // clear all server session information about this user
 router.get('/logout', function(req, res) {
@@ -48,46 +57,64 @@ router.get('/logout', function(req, res) {
 
 //delete route
 router.delete('/:id', function(req, res) {
-    console.log('delete hit', req.params.id);
-    pool.connect(function(err, db, done) {
-        if (err) {
-            console.log('delete error: ', err);
-            res.sendStatus(500);
-        } else {
-            db.query('DELETE FROM users WHERE id=$1', [req.params.id], function(errorMakingQuery, result) {
-                done();
-                if (errorMakingQuery) {
-                    console.log('error with put', errorMakingQuery);
-                    res.sendStatus(500);
-                } else {
-                    res.sendStatus(200);
-                }
-            })
-        }
-    })
+    if (req.isAuthenticated()) {
+        // send back user object from database
+        console.log('logged in', req.user);
+        console.log('delete hit', req.params.id);
+        pool.connect(function(err, db, done) {
+            if (err) {
+                console.log('delete error: ', err);
+                res.sendStatus(500);
+            } else {
+                db.query('DELETE FROM users WHERE id=$1', [req.params.id], function(errorMakingQuery, result) {
+                    done();
+                    if (errorMakingQuery) {
+                        console.log('error with put', errorMakingQuery);
+                        res.sendStatus(500);
+                    } else {
+                        res.sendStatus(200);
+                    }
+                })
+            }
+        })
+    } else {
+        // failure best handled on the server. do redirect here.
+        console.log('not logged in');
+        // should probably be res.sendStatus(403) and handled client-side, esp if this is an AJAX request (which is likely with AngularJS)
+        res.sendStatus(403);
+    }
 }); //end delete route
 
 
 //admin edit route//
 
 router.put('/', function(req, res) {
-    console.log('put hit', req.body);
-    pool.connect(function(err, db, done) {
-        if (err) {
-            console.log('remove error: ', err);
-            res.sendStatus(500);
-        } else {
-            db.query('UPDATE users SET first_name=$1, last_name=$2, position=$3, email=$4 WHERE id=$5', [req.body.first_name, req.body.last_name, req.body.position, req.body.email, req.body.id], function(errorMakingQuery, result) {
-                done();
-                if (errorMakingQuery) {
-                    console.log('error with put', errorMakingQuery);
-                    res.sendStatus(500);
-                } else {
-                    res.sendStatus(200);
-                }
-            })
-        }
-    })
+    if (req.isAuthenticated()) {
+        // send back user object from database
+        console.log('logged in', req.user);
+        console.log('put hit', req.body);
+        pool.connect(function(err, db, done) {
+            if (err) {
+                console.log('remove error: ', err);
+                res.sendStatus(500);
+            } else {
+                db.query('UPDATE users SET first_name=$1, last_name=$2, position=$3, email=$4 WHERE id=$5', [req.body.first_name, req.body.last_name, req.body.position, req.body.email, req.body.id], function(errorMakingQuery, result) {
+                    done();
+                    if (errorMakingQuery) {
+                        console.log('error with put', errorMakingQuery);
+                        res.sendStatus(500);
+                    } else {
+                        res.sendStatus(200);
+                    }
+                })
+            }
+        })
+    } else {
+        // failure best handled on the server. do redirect here.
+        console.log('not logged in');
+        // should probably be res.sendStatus(403) and handled client-side, esp if this is an AJAX request (which is likely with AngularJS)
+        res.sendStatus(403);
+    }
 });
 //end admin edit route//
 
