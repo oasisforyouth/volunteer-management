a587a7d7772d4e5e8b42bfb1c4eac0834dd511da
Nic Bade <chipchapmaniii@gmail.com>
2017-10-09 08:58:00 -0500

    removed authentication on email application route

--- server/routes/email.router.js
+++ server/routes/email.router.js
@@ -7,39 +7,39 @@ let nodemailer = require('nodemailer');
 // NEW VOLUNTEER APPLICAITON EMAIL
 router.post('/', (req, res, next) => {
     // create reusable transporter object using the default SMTP transport
-    if (req.isAuthenticated()) {
-        console.log('logged in email', req.user);
+    // if (req.isAuthenticated()) {
+    //     console.log('logged in email', req.user);
 
-        let transporter = nodemailer.createTransport({
-            service: 'gmail',
-            auth: {
-                user: process.env.DB_USER,
-                pass: process.env.DB_PASS
-            }
-        });
+    let transporter = nodemailer.createTransport({
+        service: 'gmail',
+        auth: {
+            user: process.env.DB_USER,
+            pass: process.env.DB_PASS
+        }
+    });
 
-        // setup email data with unicode symbols
-        let mailOptions = {
-            from: '"Administrator"' + process.env.DB_EMAIL, // sender address  
-            // THIS WILL BE JESS' EMAIL ADDRESS
-            to: process.env.DB_PM_EMAIL, // list of receivers 
-            subject: 'Volunteer Application', // Subject line
-            // JESS CAN PERSONALIZE THIS IF SHE WOULD LIKE US TO
-            text: 'A new Volunteer application has been submitted', // plain text body
-        };
+    // setup email data with unicode symbols
+    let mailOptions = {
+        from: '"Administrator"' + process.env.DB_EMAIL, // sender address  
+        // THIS WILL BE JESS' EMAIL ADDRESS
+        to: process.env.DB_PM_EMAIL, // list of receivers 
+        subject: 'Volunteer Application', // Subject line
+        // JESS CAN PERSONALIZE THIS IF SHE WOULD LIKE US TO
+        text: 'A new Volunteer application has been submitted', // plain text body
+    };
 
-        // send mail with defined transport object
-        transporter.sendMail(mailOptions, (error, info) => {
-            if (error) {
-                return console.log(error);
-            }
-            console.log('Message sent: %s', info.messageId);
-            console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
-        });
-    } else {
-        console.log('not logged in');
-        res.sendstatus(403);
-    }
+    // send mail with defined transport object
+    transporter.sendMail(mailOptions, (error, info) => {
+        if (error) {
+            return console.log(error);
+        }
+        console.log('Message sent: %s', info.messageId);
+        console.log('Preview URL: %s', nodemailer.getTestMessageUrl(info));
+    });
+    // } else {
+    //     console.log('not logged in');
+    //     res.sendStatus(403);
+    // }
 });
 
 
@@ -77,7 +77,7 @@ router.post('/user', (req, res, next) => {
         });
     } else {
         console.log('not logged in');
-        res.sendstatus(403);
+        res.sendStatus(403);
     }
 });
 
