5be9acaa475586702efe57cc56f022127311b772
Tom Ritland <t_ritland@yahoo.com>
2017-10-12 10:52:11 -0500

    navbar works

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -11,15 +11,15 @@ myApp.config(function($routeProvider, $locationProvider, $mdThemingProvider) {
         })
         .when('/login', {
             templateUrl: '/views/templates/login.html',
-            controller: 'LoginController as lc',
+            controller: 'LoginController as vm',
         })
         .when('/application', {
             templateUrl: '/views/templates/application.html',
-            controller: 'ApplicationController as ac',
+            controller: 'ApplicationController as vm',
         })
         .when('/user', {
             templateUrl: '/views/templates/user.html',
-            controller: 'UserController as uc',
+            controller: 'UserController as vm',
             resolve: {
                 getuser: function(UserService) {
                     return UserService.getuser();
@@ -37,7 +37,7 @@ myApp.config(function($routeProvider, $locationProvider, $mdThemingProvider) {
         })
         .when('/newAdmin/:id', {
             templateUrl: '/views/templates/newAdmin.html',
-            controller: 'LoginController as lc',
+            controller: 'LoginController as vm',
             // DO I NEED THIS?
             // resolve: {
             //     getuser: function(UserService) {
@@ -74,7 +74,7 @@ myApp.config(function($routeProvider, $locationProvider, $mdThemingProvider) {
         })
         .when('/newAdminLogin', {
             templateUrl: '/views/templates/newAdminLogin.html',
-            controller: 'LoginController as lc',
+            controller: 'LoginController as vm',
             resolve: {
                 getuser: function(UserService) {
                     return UserService.getuser();
@@ -83,7 +83,7 @@ myApp.config(function($routeProvider, $locationProvider, $mdThemingProvider) {
         })
         .when('/volunteerDetail/:id', {
             templateUrl: '/views/templates/volunteerDetail.html',
-            controller: 'VolunteerDetailController as vc',
+            controller: 'VolunteerDetailController as vm',
             resolve: {
                 getuser: function(UserService) {
                     return UserService.getuser();
