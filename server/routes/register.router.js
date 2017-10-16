c3bed6855560efb4f97d29c7310de8643956026b
Nic Bade <chipchapmaniii@gmail.com>
2017-10-16 14:19:04 -0500

    password is now secure

--- server/routes/register.router.js
+++ server/routes/register.router.js
@@ -31,18 +31,19 @@ router.post('/', function(req, res, next) {
             if (err) {
                 console.log("Error connecting: ", err);
                 res.sendStatus(500);
-            }
-            client.query("INSERT INTO users (first_name, last_name, user_name, email, password, position) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id", [saveUser.firstname, saveUser.lastname, saveUser.username, saveUser.email, saveUser.password, saveUser.position],
-                function(err, result) {
-                    client.end();
+            } else {
+                client.query("INSERT INTO users (first_name, last_name, user_name, email, password, position) VALUES ($1, $2, $3, $4, $5, $6) RETURNING id", [saveUser.firstname, saveUser.lastname, saveUser.username, saveUser.email, saveUser.password, saveUser.position],
+                    function(err, result) {
+                        client.end();
 
-                    if (err) {
-                        console.log("Error inserting data: ", err);
-                        res.sendStatus(500);
-                    } else {
-                        res.sendStatus(201);
-                    }
-                });
+                        if (err) {
+                            console.log("Error inserting data: ", err);
+                            res.sendStatus(500);
+                        } else {
+                            res.sendStatus(201);
+                        }
+                    });
+            }
         });
     } else {
         // failure best handled on the server. do redirect here.
@@ -71,7 +72,7 @@ router.put('/:id', function(req, res, next) {
                         console.log("Error inserting data: ", err);
                         res.sendStatus(500);
                         // MATCHES HASH WITH ROUTE PARAMS, IF THE SAME WE CAN PROCEED
-                    } else if (result < 1) {
+                    } else if (result.rows.length < 1) {
                         res.sendStatus(500);
                     } else {
                         pool.connect(function(err, client, done) {
@@ -83,57 +84,61 @@ router.put('/:id', function(req, res, next) {
                             client.query("SELECT * FROM crypto WHERE md5=$1;", [adminId],
                                 function(err, result) {
                                     console.log('result.rows: ', result.rows);
-                                    res.send(result.rows);
                                     client.end();
-                                    pool.connect(function(err, client, done) {
-                                        if (err) {
-                                            console.log("Error connecting: ", err);
-                                            res.sendStatus(500);
-                                        } else {
-                                            client.query("UPDATE crypto SET active=$1 WHERE md5=$2;", [active, adminId],
-                                                function(err, result) {
-                                                    console.log('result: ', result);
-                                                    // res.sendStatus(201);
-                                                    client.end();
-                                                });
-                                            if (adminId == result.rows[0].md5 && result.rows[0].active == true) {
-                                                // console.log('HOOORAY');
-                                                pool.connect(function(err, client, done) {
-                                                    var saveUser = {
-                                                        firstname: req.body.firstName,
-                                                        lastname: req.body.lastName,
-                                                        username: req.body.username,
-                                                        email: req.body.email,
-                                                        password: encryptLib.encryptPassword(req.body.password),
-                                                        position: req.body.position,
-                                                    };
-                                                    console.log('saveUser: ', saveUser)
-
-                                                    if (err) {
-                                                        // console.log("Error connecting post: ", err);
-                                                        res.sendStatus(500);
-                                                    } else {
-
-
-                                                        client.query("INSERT INTO users (first_name, last_name, user_name, email, password, position) VALUES ($1, $2, $3, $4, $5, $6);"), [saveUser.firstname, saveUser.lastname, saveUser.username, saveUser.email, saveUser.password, saveUser.position],
-                                                            function(err, result) {
-                                                                console.log('result: ', result.rows);
-                                                                client.end();
+                                    if (err) {
+                                        console.log('error selecting from crypto', err)
+                                    } else {
+                                        pool.connect(function(err, client, done) {
+                                            if (err) {
+                                                console.log("Error connecting: ", err);
+                                                res.sendStatus(500);
+                                            } else {
+                                                client.query("UPDATE crypto SET active=FALSE WHERE md5=$1 AND active=TRUE returning md5;", [adminId],
+                                                    function(err, result) {
+                                                        client.end();
+                                                        console.log('result: ', result);
+                                                        if (adminId == result.rows[0].md5) {
+                                                            // console.log('HOOORAY');
+                                                            pool.connect(function(err, client, done) {
+                                                                var saveUser = {
+                                                                    firstname: req.body.firstName,
+                                                                    lastname: req.body.lastName,
+                                                                    username: req.body.username,
+                                                                    email: req.body.email,
+                                                                    password: encryptLib.encryptPassword(req.body.password),
+                                                                    position: req.body.position,
+                                                                };
+                                                                console.log('saveUser: ', saveUser)
 
                                                                 if (err) {
-                                                                    console.log("Error inserting data: ", err);
+                                                                    // console.log("Error connecting post: ", err);
                                                                     res.sendStatus(500);
                                                                 } else {
-                                                                    res.sendStatus(201);
+
+
+                                                                    client.query("INSERT INTO users (first_name, last_name, user_name, email, password, position) VALUES ($1, $2, $3, $4, $5, $6);", [saveUser.firstname, saveUser.lastname, saveUser.username, saveUser.email, saveUser.password, saveUser.position], function(err, result) {
+                                                                        console.log('result: ', result.rows);
+                                                                        client.end();
+
+                                                                        if (err) {
+                                                                            console.log("Error inserting data: ", err);
+                                                                            res.sendStatus(500);
+                                                                        } else {
+                                                                            res.sendStatus(201);
+                                                                        }
+
+                                                                    });
                                                                 }
+                                                            });
+                                                        }
+                                                        // res.sendStatus(201);
 
-                                                            }
-                                                    }
-                                                });
+                                                    });
+
                                             }
-                                        }
 
-                                    });
+                                        });
+                                    }
                                 })
 
                         }); // end pool.connect
